3b01fdca541102b5125da27fedd14e6b5ebf5456
marcosqf93 <quintana.mqf@gmail.com>
2026-08-04 10:07:35 -0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const defaultPrices = {
 if (sendgridApiKey) sendgridMail.setApiKey(sendgridApiKey);
 
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(cors({ origin: frontendUrl === '*' ? true : frontendUrl, credentials: true }));
 
 function createTempCode() {
